ad3f24e59fecb541c2026c085e0fd7d9a09a95de
Rafa Ramírez <raffe90@gmail.com>
2015-12-14 19:51:58 +0100

    Fixes on pdf layout file

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -15,7 +15,6 @@ WickedPdf.config = {
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-  wkhtmltopdf:                    '/usr/bin/wkhtmltopdf', # path to binary
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
   #
