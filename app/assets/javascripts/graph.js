10e87486b1ae2f1c0fb8027dc97fb51ab599be25
Rafa Ramírez <raffe90@gmail.com>
2016-01-23 20:41:13 +0100

    Working on showing and hiding lines

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -25,77 +25,31 @@ $(document).ready(function(e){
 });
 
 
-  function drawGraphFromSession(){
-    $.ajax({
-      type: "GET",
-      contentType: "application/json; charset=utf-8",
-      url: '/cholesterol-session',
-      dataType: 'json',
-      success: function (data) {
-        // console.log(data);
-        drawMultiLine({entries: data.entries, interventions: data.intervention});
-      },
-      error: function (result) {
-         error();
-      }
-    });
-  }
-
-
-// function draw(data) {
-//     var color = d3.scale.category20b();
-//     console.log(color);
-//     var width = 420,
-//         barHeight = 20;
-
-//     var x = d3.scale.linear()
-//         .range([0, width])
-//         .domain([0, d3.max(data)]);
-
-//     var chart = d3.select("#graph")
-//         .attr("width", width)
-//         .attr("height", barHeight * data.length);
-
-//     var bar = chart.selectAll("g")
-//         .data(data)
-//         .enter().append("g")
-//         .attr("transform", function (d, i) {
-//                   return "translate(0," + i * barHeight + ")";
-//               });
-
-//     bar.append("rect")
-//         .attr("width", x)
-//         .attr("height", barHeight - 1)
-//         .style("fill", function (d) {
-//                    return color(d)
-//                })
-
-//     bar.append("text")
-//         .attr("x", function (d) {
-//                   return x(d) - 10;
-//               })
-//         .attr("y", barHeight / 2)
-//         .attr("dy", ".35em")
-//         .style("fill", "white")
-//         .text(function (d) {
-//           return d;
-//         });
-// }
+function drawGraphFromSession(){
+  $.ajax({
+    type: "GET",
+    contentType: "application/json; charset=utf-8",
+    url: '/cholesterol-session',
+    dataType: 'json',
+    success: function (data) {
+      // console.log(data);
+      drawMultiLine({entries: data.entries, interventions: data.intervention});
+    },
+    error: function (result) {
+       error();
+    }
+  });
+}
 
 function drawMultiLine(data) {
-// console.log("call");
-// Set the dimensions of the canvas / graph
-var margin = {top: 30, right: 20, bottom: 70, left: 50},
-    width = 768 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+  var margin = {top: 30, right: 20, bottom: 70, left: 50},
+      width = 768 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
 
-// Parse the date / time
-var parseDate = d3.time.format("%b %Y").parse;
+  var parseDate = d3.time.format("%b %Y").parse;
 
-var drag = d3.behavior.drag()
+  var drag = d3.behavior.drag()
     .on("drag", function(d,i) {
-        // x(d.date) += d3.event.dx
-        // d.value += 40
         d.value -= d3.event.dy
         d3.select(this).attr("transform", function(d,i){
           if(d.symbol === 'HDL' || d.symbol === 'LDL'){
@@ -104,7 +58,6 @@ var drag = d3.behavior.drag()
           else{
             return "translate("+(x(d.date)-7.5)+","+(y(d.value)-10)+")";
           }
-            // return "translate(" + [ x(d.date), y(d.value) ] + ")"
         })
     });
 
@@ -114,23 +67,16 @@ var y = d3.scale.linear().range([height, 0]);
 
 // Define the axes
 var xAxis = d3.svg.axis().scale(x)
-    // .ticks(data.entries.length/4)
     .tickFormat(d3.time.format("%m/%y"))
     .orient("bottom");
 
 var yAxis = d3.svg.axis().scale(y)
-  // .tickPadding(10)
   .ticks(data.entries.length/4)
-  // .tickSize(-width)
-  // .tickSubdivide(true)
-    .orient("left");
+  .orient("left");
 
 // Define the line
 var priceline = d3.svg.line()
-    .x(function(d) {
-      // console.log(d);
-      return x(d.date);
-    })
+    .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.value); });
 
 // Adds the svg canvas
@@ -147,8 +93,6 @@ var svg = d3.select("#graph")
       d.date = parseDate(d.date);
       d.value = +d.value;
     });
-
-
 
     // Scale the range of the data
     var minDate = new Date(data.entries[0].date.getFullYear()-1, data.entries[0].date.getMonth()+1,data.entries[0].date.getDate());
@@ -176,8 +120,6 @@ var svg = d3.select("#graph")
 
     // Loop through each symbol / key
     dataNest.forEach(function(d,i) {
-         console.log(th(d.key));
-
         svg.append("clipPath")
               .attr("id", "clip-above")
             .append("rect")
@@ -191,58 +133,61 @@ var svg = d3.select("#graph")
               .attr("width", width)
               .attr("height", height - y(th(d.key)));
 
-          var clip = ["above", "below"];
-
-
-
-          // console.log(i, th[i], color(d.key), d.key);
+          var clip = ["below", "above"];
 
           clip.forEach(function(v,i){
             svg.append("path")
               .attr("class", function() { return 'tag'+d.key.replace(/\s+/g, '') + " line " + v; })
               .attr("clip-path", function() { return "url(#clip-" + v + ")"; })
-              .attr("id", 'tag'+d.key.replace(/\s+/g, '')) // assign ID
+              // .attr("id", 'tag'+d.key.replace(/\s+/g, ''))
               .attr("d", priceline(d.values))
+              .style("stroke-dasharray", function(){
+                  if(i === 0){
+                    return ("0, 0");
+                  }
+                  else{
+                    return ("3, 3");
+                  }
+              })
               .style("stroke", function() { // Add the colours dynamically
-                  return d.color = color(d.key); })
-              .on("click", function(){
-                  var active   = d.active ? false : true;
-                  newOpacity = active ? 0 : 1;
-                  // Hide or show the elements based on the ID
-                  // d3.select("#tag"+d.key.replace(/\s+/g, ''))
-                  //     .transition().duration(100)
-                  //     .style("opacity", newOpacity);
-                  d3.selectAll(".tag"+d.key.replace(/\s+/g, ''))
-                      .transition().duration(100)
-                      .style("opacity", newOpacity);
-                  // Update whether or not the elements
-                  d.active = active;
+                  return color(d.key);
               });
-
           });
 
         // Add the Legend
         svg.append("text")
-            .attr("x", width + margin.left + 50)  // space legend
-            .attr("y", 100 + i * (margin.bottom/2)+ 5)
-            .attr("class", "legend")    // style the legend
-            .style("fill", function() { // Add the colours dynamically
-                return d.color = color(d.key); })
-            .on("click", function(){
-                // Determine if current line is visible
-                var active   = d.active ? false : true;
-                newOpacity = active ? 0 : 1;
-                // Hide or show the elements based on the ID
-                d3.select("#tag"+d.key.replace(/\s+/g, ''))
-                    .transition().duration(100)
-                    .style("opacity", newOpacity);
-                d3.selectAll(".tag"+d.key.replace(/\s+/g, ''))
-                    .transition().duration(100)
-                    .style("opacity", newOpacity);
-                // Update whether or not the elements are active
-                d.active = active;
+            .attr("x", (legendSpace/2)+i*legendSpace)
+            .attr("y", height + (margin.bottom/2)+ 5)
+            .attr("class", "legend")
+            .style("fill", function() {
+                return d.color = color(d.key);
             })
-            .text(d.key);
+            .text(d.key)
+            .on("click", function(){
+                var active   = d.active ? false : true;
+                newOpacity = active ? 0.05 : 1;
+                d3.selectAll(".tag"+d.key.replace(/\s+/g, '')+'.line.above')
+                    .transition().duration(500)
+                    .style("opacity", newOpacity)
+                    .style("stroke-dasharray", function(){
+                      if(active){
+                        return ("0, 0");
+                      }
+                      else{
+                        return ("5, 5");
+                      }
+                    });
+                d3.selectAll(".tag"+d.key.replace(/\s+/g, '')+'.line.below')
+                    .transition().duration(500)
+                    .style("opacity", newOpacity);
+                d3.selectAll(".dots.tag"+d.key.replace(/\s+/g, ''))
+                    .transition().duration(250)
+                    .style("opacity", newOpacity);
+                d3.selectAll(".text-values.tag"+d.key.replace(/\s+/g, ''))
+                    .transition().duration(250)
+                    .style("opacity", newOpacity);
+                d.active = active;
+            });
 
     });
 
@@ -423,26 +368,25 @@ var xAxis = d3.svg.axis().scale(x)
     .orient("bottom");
 
 var yAxis = d3.svg.axis().scale(y)
-  // .tickPadding(10)
-  // .ticks(data.entries.length/4)
-  // .tickSize(-width)
-  // .tickSubdivide(true)
     .orient("left");
+
     data.entries.forEach(function(d) {
       d.date = parseDate(d.date);
       d.value = +d.value;
     });
 
-// Define the line
-var priceline = d3.svg.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.value); });
+  // Define the line
+  var priceline = d3.svg.line()
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.value); });
+
     // Scale the range of the data
-    x.domain(d3.extent(data.entries, function(d) { return d.date; }));
-    y.domain([0, d3.max(data.entries, function(d) { return d.value; })+75]);
+    var minDate = new Date(data.entries[0].date.getFullYear()-1, data.entries[0].date.getMonth()+1,data.entries[0].date.getDate());
+    var maxDate = new Date(data.entries[data.entries.length - 1].date.getFullYear()+1, data.entries[data.entries.length - 1].date.getMonth()+1,data.entries[data.entries.length - 1].date.getDate());
 
-      // console.log(d3.select("#graph").selectAll('rect').empty());
-
+    x.domain([minDate, maxDate]);
+    // x.domain(d3.extent(data.entries, function(d) { return d.date; }));
+    y.domain([d3.min(data.entries, function(d) { return d.value; })-75, d3.max(data.entries, function(d) { return d.value; })+75]);
 
         var parseInterventionDate = d3.time.format("%Y-%m-%d").parse;
         data.interventions.forEach(function(d) {
@@ -455,10 +399,12 @@ var priceline = d3.svg.line()
 
     // if(d3.select("#graph").selectAll('rect').empty()){
         var svg = d3.select("svg");
-        var rect = svg.selectAll("rect").data(data.interventions);
+        var rect = svg.selectAll(".new-interventions").data(data.interventions);
         var rectEnter = rect.enter().append("rect");
+
         rectEnter.style("opacity", 0.1)
         .attr('width', function(d,i){
+            console.log(d);
             return x(d.end)-x(d.start);
         })
         .attr('x', function(d) {
@@ -478,7 +424,7 @@ var priceline = d3.svg.line()
           .enter()
           .append("text")
           .html(function(d){
-            console.log(d.title);
+            // console.log(d.title);
             return d.title+" - "+d.description;
           })
           .style("opacity", 1)
