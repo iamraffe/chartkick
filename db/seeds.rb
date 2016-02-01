7430a46fc8f4f5e3641e69f8b06ceabd7e498fd4
raffe90 <raffe90@gmail.com>
2016-02-01 17:54:04 +0100

    Adds more users to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 
 # 6.times{Cholesterol.create({date: Faker::Date.backward(3650), ldl: Faker::Number.between(1, 500), hdl: Faker::Number.between(1, 500), triglycerides: Faker::Number.between(1, 500), cholesterol: Faker::Number.between(1, 500)})}
 
-40.times{User.create(name: Faker::Name.name, gender: ["M", "F"].sample, diabetes: [true, false].sample, heart_disease: [true, false].sample, date_of_birth: Faker::Time.between(50.years.ago, 35.years.ago))}
+300.times{User.create(name: Faker::Name.name, gender: ["M", "F"].sample, diabetes: [true, false].sample, heart_disease: [true, false].sample, date_of_birth: Faker::Time.between(50.years.ago, 35.years.ago))}
