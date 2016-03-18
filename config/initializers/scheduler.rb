a685de63a4defff469d53b062e80b28f3a493a05
raffe90 <raffe90@gmail.com>
2016-03-18 21:56:50 +0100

    Fix on scheduler

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -8,19 +8,18 @@ s = Rufus::Scheduler.singleton
 
 # Awesome recurrent task...
 #
-s.every '5m' do
+s.every '1h' do
   auth = {:username => ENV["api_username"], :password => ENV["api_password"]}
   @patients = HTTParty.get("https://api.test.elationemr.com/api/v1/patients/", 
                        :basic_auth => auth, verify: false).parsed_response["results"]
   @patients.each do |patient|
-    p = User.where(elation_id: patient["id"])
+    p = User.where(elation_id: patient["id"]).first
     if p.nil?
+      Rails.logger.debug "hello, it's #{patient['id']}"
       # p["elation_id"] = p.delete "id"
-      User.create(password: "", password_confirmation: "", 
-                  elation_id: patient["id"], email:  patient["email"],
-                  name:  patient["first_name"], elation_payload: patient.to_json)
+      User.create!(password: "n3wp4t13n1", password_confirmation: "n3wp4t13n1", elation_id: patient["id"], email:  patient["email"].blank? ? "example@email.com" : patient["email"], name:  patient["first_name"], elation_payload: patient.to_json)
     else
-      p.elation_payload = p.to_json
+      p.elation_payload = patient.to_json
       p.save
     end
   end
