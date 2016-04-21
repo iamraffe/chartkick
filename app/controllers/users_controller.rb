b4906c1d3237e1eb1e6557367f316f2857366ee4
Rafa Ramírez <raffe90@gmail.com>
2016-04-22 01:44:19 +0200

    Adds some controller refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
   autocomplete :user, :name, :full => true, :extra_data => [:id]
 
   def index
-    if params[:user][:id].blank?
-    else
+    unless params[:user][:id].blank?
       redirect_to user_path(params[:user][:id])
     end
   end
