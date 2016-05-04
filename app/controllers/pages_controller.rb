acd9112ea27a346779c9b3496f4834fd3a9abe3e
Rafa Ramírez <raffe90@gmail.com>
2016-05-05 00:00:01 +0200

    Cleans session upon dash

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
 
   def dashboard
+    SessionHelper.clean(session)
     @notifications = Notification.where(receiver_id: current_user.id).order(created_at: :desc).limit(10)
     @messages = []
     @patients = current_user.primary_care_team.patients
