e60e1032b1b6822e131cf5772b53a46ec6a0eec6
Rafa Ramírez <raffe90@gmail.com>
2016-06-07 00:53:54 +0200

    Adds fix to update_all notifications

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -35,38 +35,42 @@ d.every '1m' do
   @users.each do |user|
     case user.notify_every
       when "1.day"
-        @notifications = Notification.where(delivered: false).where(receiver_id: user.id).update_all(delivered: true)
+        @notifications = Notification.where(delivered: false).where(receiver_id: user.id)
         if @notifications.size > 0
           UserMailer.welcome_email(user, @notifications.to_a).deliver_later
+          @notifications.update_all(delivered: true)
           user.update_attributes(notify_by: (DateTime.current+1.day).change({hour: 7}))
         end
       when "weekdays"
         today =  DateTime.current.wday
         if(today != 0 && today != 6)
-          @notifications = Notification.where(delivered: false).where(receiver_id: user.id).update_all(delivered: true)
+          @notifications = Notification.where(delivered: false).where(receiver_id: user.id)
           if @notifications.size > 0
             UserMailer.welcome_email(user, @notifications.to_a).deliver_later
             notify_by = DateTime.current.friday? ? DateTime.current+3.day : DateTime.current+1.day
+            @notifications.update_all(delivered: true)
             user.update_attributes(notify_by: notify_by.change({hour: 7}))
           end
         end
       when "3.times.week"
         today =  DateTime.current.wday
         if(today == 1 || today == 3 || today == 5)
-          @notifications = Notification.where(delivered: false).where(receiver_id: user.id).update_all(delivered: true)
+          @notifications = Notification.where(delivered: false).where(receiver_id: user.id)
           if @notifications.size > 0
             UserMailer.welcome_email(user, @notifications.to_a).deliver_later
             notify_by = today == 5 ? DateTime.current+3.day : DateTime.current+2.day
+            @notifications.update_all(delivered: true)
             user.update_attributes(notify_by: notify_by.change({hour: 7}))
           end
         end
       when -> (n) { (0...6).include? n.to_i }
         today =  DateTime.current.wday
         if(today == notify_every.to_i)
-          @notifications = Notification.where(delivered: false).where(receiver_id: user.id).update_all(delivered: true)
+          @notifications = Notification.where(delivered: false).where(receiver_id: user.id)
           if @notifications.size > 0
             UserMailer.welcome_email(user, @notifications.to_a).deliver_later
             notify_by = DateTime.current+7.day
+            @notifications.update_all(delivered: true)
             user.update_attributes(notify_by: notify_by.change({hour: 7}))
           end
         end
